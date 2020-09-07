1fdd9bb65430fab382f6494c199544f3d8cd8fe2
Erick Wendel <erick.workspace@gmail.com>
2020-09-07 15:33:51 -0300

    initial commit
    Signed-off-by: Erick Wendel <erick.workspace@gmail.com>

---
+++ app.js
@@ -0,0 +1,50 @@
+/*
+Example:
+    name=Zé zinho&profession=Node.js&email=eti@ttdude.com&phone=+00000&website=aaee.erickwendel.com&location=Jo Paulo, SP
+    https://www.convertonline.io/convert/json-to-query-string
+*/
+const defaultValues = {
+    "name": "Erick Wendel",
+    "profession": "Node.js Instructor",
+    "email": "ei@dude.com",
+    "phone": "+447795001178",
+    "website": "cursos.erickwendel.com",
+    "location": "São Paulo, SP"
+}
+
+function getQueryString() {
+    const search = location.search.substring(1);
+    if (!search) return null;
+    const uri = decodeURI(search)
+        .replace(/"/g, '\\"')
+        .replace(/&/g, '","')
+        .replace(/=/g, '":"')
+
+    const data = JSON.parse(`{"${uri}"}`);
+    return data;
+}
+
+function updateData(data) {
+    Object.keys(data).forEach(key => {
+        document.getElementById(key).innerHTML = data[key]
+    })
+}
+
+window.onload = () => {
+    const qs = getQueryString()
+    if (!qs) {
+        console.warn('queryString not found using default values...')
+    }
+    const getLength = obj => Object.keys(obj).length
+    const isMissingKeysOnQueryString = getLength(defaultValues) === getLength(qs)
+    if (!isMissingKeysOnQueryString) {
+        console.warn(`All missing parameters will come from default values\nRequired parameters are`, Object.keys(defaultValues))
+    }
+
+    
+    updateData({
+        ...defaultValues,
+        ...qs,
+    })
+
+}
